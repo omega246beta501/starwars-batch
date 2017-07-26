a68a29e9fdb24dd89420c869eccef73b0a8463e9
Ruben Orenes <ruben_ayg@hotmail.com>
2017-07-27 00:40:21 +0200

    Ejercicio HelloWorld

--- src/main/java/com/starwars/ApiApplication.java
+++ src/main/java/com/starwars/ApiApplication.java
@@ -1,12 +1,12 @@
-package com.starwars.batch;
+package com.starwars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StarwarsBatchApplication {
+public class ApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StarwarsBatchApplication.class, args);
+		SpringApplication.run(ApiApplication.class, args);
 	}
 }
---
+++ src/main/java/com/starwars/config/BatchConfiguration.java
@@ -0,0 +1,33 @@
+package com.starwars.config;
+
+import com.starwars.tasklet.HelloWorldTasklet;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration {
+    @Bean
+    public Step helloWorldStep(StepBuilderFactory stepBuilderFactory, HelloWorldTasklet helloWorldTasklet) {
+        return stepBuilderFactory
+                .get("helloWorldStep")
+                .tasklet(helloWorldTasklet)
+                .build();
+    }
+
+    @Bean
+    public Job job(JobBuilderFactory jobBuilderFactory, Step helloWorldStep) {
+        return jobBuilderFactory
+                .get("job")
+                .incrementer(new RunIdIncrementer())
+                .start(helloWorldStep)
+                .build();
+    }
+
+}
